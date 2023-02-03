3e0c4dda37cdde10a8b1b72519a60b02d96127a6
jac-oblong <jacoblong853@gmail.com>
2023-02-03 17:52:54 -0500

    Switch to using smart pointers

--- include/glibby/spacial-algorithms/QuadTree.h
+++ include/glibby/spacial-algorithms/QuadTree.h
@@ -3,33 +3,33 @@
 
 #include "glibby/primitives/point2D.h"
 
+#include <memory>
+
 namespace glibby {
   class QuadTreeNode {
     public:
-      QuadTreeNode(Point2D* cen, float width, float height, int cap);
+      QuadTreeNode(std::shared_ptr<Point2D> cen, float width, float height, int cap);
 
-      Point2D* center_;
+      std::shared_ptr<Point2D> center_;
       int capacity_;
+      int num_points_;
       float width_;
       float height_; 
-      QuadTreeNode* NW_;
-      QuadTreeNode* NE_;
-      QuadTreeNode* SW_;
-      QuadTreeNode* SE_;
-      QuadTreeNode* parent_; 
-      Point2D* points_;
+      std::shared_ptr<QuadTreeNode> NW_;
+      std::shared_ptr<QuadTreeNode> NE_;
+      std::shared_ptr<QuadTreeNode> SW_;
+      std::shared_ptr<QuadTreeNode> SE_;
+      std::shared_ptr<QuadTreeNode> parent_; 
+      std::shared_ptr<Point2D> points_;
   };
 
   class QuadTree {
     public:
-    QuadTree(Point2D* TopL, Point2D* TopR, Point2D* BotL, Point2D* BotR, int capacity = 1);
-    QuadTree(Point2D* p, float width, float height, int capacity = 1);
-    ~QuadTree();
+      QuadTree(std::shared_ptr<Point2D> TopL, std::shared_ptr<Point2D> TopR, std::shared_ptr<Point2D> BotL, std::shared_ptr<Point2D> BotR, int capacity = 1);
+      QuadTree(std::shared_ptr<Point2D> p, float width, float height, int capacity = 1);
 
     private:
-      void destroy_sub_tree(QuadTreeNode* ptr);
-
-      QuadTreeNode* ptr_;
+      std::shared_ptr<QuadTreeNode> ptr_;
       int capacity_;
   };
 }
--- src/spacial-algorithms/QuadTree.cpp
+++ src/spacial-algorithms/QuadTree.cpp
@@ -1,51 +1,33 @@
 #include "glibby/spacial-algorithms/QuadTree.h"
 #include "glibby/primitives/point2D.h"
 
+#include <memory>
 
 namespace glibby {
-  QuadTreeNode::QuadTreeNode(Point2D* cen, float width, float height, int cap) {
+  QuadTreeNode::QuadTreeNode(std::shared_ptr<Point2D> cen, float width, float height, int cap) {
     this->center_ = cen;
     this->width_ = width;
     this->height_ = height;
     this->capacity_ = cap;
+    this->num_points_ = 0;
   }
 
-  QuadTree::QuadTree(Point2D* TopL, Point2D* TopR, Point2D* BotL, Point2D* BotR, int capacity) {
+  QuadTree::QuadTree(std::shared_ptr<Point2D> TopL, std::shared_ptr<Point2D> TopR, std::shared_ptr<Point2D> BotL, std::shared_ptr<Point2D> BotR, int capacity) {
     this->capacity_ = capacity;
 
     // using points to create rectangle that will be the bounding box
     float width = TopR->x - TopL->x;
     float height = TopR->y - BotR->y;
     
-    Point2D* temp = new Point2D;
+    std::shared_ptr<Point2D> temp( new Point2D);
     temp->x = BotR->x + width/2;
     temp->y = BotR->y + height/2;
 
-    this->ptr_ = new QuadTreeNode(temp, width, height, capacity);
+    this->ptr_ = std::shared_ptr<QuadTreeNode>(new QuadTreeNode(temp, width, height, capacity));
   }
    
-  QuadTree::QuadTree(Point2D* p, float width, float height, int capacity) {
-    this->ptr_ = new QuadTreeNode(p,width,height,capacity);
+  QuadTree::QuadTree(std::shared_ptr<Point2D> p, float width, float height, int capacity) {
+    this->ptr_ = std::shared_ptr<QuadTreeNode>(new QuadTreeNode(p,width,height,capacity));
     this->capacity_ = capacity;
-  }
-
-  QuadTree::~QuadTree(){
-    destroy_sub_tree(this->ptr_);
-  }
-
-  
-
-  void QuadTree::destroy_sub_tree(QuadTreeNode* ptr) {
-    // if ptr is NULL we stop recursing
-    if (ptr == 0) {
-      return;
-    }
-    delete ptr->center_;
-    delete [] ptr->points_;
-    // recurse to all subtrees
-    destroy_sub_tree(ptr->NW_);
-    destroy_sub_tree(ptr->NE_);
-    destroy_sub_tree(ptr->SW_);
-    destroy_sub_tree(ptr->SE_);
   }
 }
