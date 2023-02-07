f418866e0a91310afe058655b2a8a8f40b751fc1
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-02-07 17:13:57 -0500

    Created examples to follow Circle and Point class

--- examples/circle.cpp
+++ examples/circle.cpp
@@ -1,11 +1,12 @@
 #include "glibby/primitives/point2D.h"
+#include "glibby/primitives/circle2D.h"
 
 #include <iostream>
 
 int main()
 {
-	glibby::Point2D p1(0.0f, 0.0f);
-	glibby::Point2D p2(3.0f, 2.0f);
+	glibby::Circle2D c1(Point2D(1, 1), 5);
+	glibby::Circle2D c2(0, 0, 5);
 
 	float distance = p1.distance(p2);
 	std::cout << distance << std::endl;
