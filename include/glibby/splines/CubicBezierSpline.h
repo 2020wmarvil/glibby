c4cffd35968a2af5a04cfc642fa60d8759560080
Hexase <wangandrew564@gmail.com>
2023-02-10 17:57:56 -0500

    added some base code

--- include/glibby/splines/CubicBezierSpline.h
+++ include/glibby/splines/CubicBezierSpline.h
@@ -9,12 +9,27 @@ namespace glibby
 		{
 			for{int i = 0; i < 4; i++}
 			{
-				points[i] = sourcepoints[i];
+				P[i] = sourcepoints[i];
 			}
 		}
-		T sampleAt(const T& at);
+		Point<T,N> sampleAt(const T& t)
+		{
+			Point<T, N> returnPoint;
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					returnPoint.points[i] =
+						(1 - t) * (1 - t) * (1 - t) * P[j].points[i] +
+						3 * (1 - t) * (1 - t) * t * +
+						3(1 - t) * t * t +
+						t * t * t 
+						//this is incomplete.
+				}
+			}
+		}
 
 	private: 
-		Point<T, N> points[4];
+		Point<T, N> P[4];
 	};
 }
