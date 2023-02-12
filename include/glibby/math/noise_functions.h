97929ace6b197076b50bd3fd7ed5a50d984e5e4f
Wyatt Marvil <2020wmarvil@gmail.com>
2023-02-11 21:37:25 -0500

    parent 781d791fe2dc955fa74fd529cf2ed295ed02349c author Wyatt Marvil <2020wmarvil@gmail.com> 1676169445 -0500 committer Hexase <wangandrew564@gmail.com> 1680297639 -0400
    parent 781d791fe2dc955fa74fd529cf2ed295ed02349c
    author Wyatt Marvil <2020wmarvil@gmail.com> 1676169445 -0500
    committer Hexase <wangandrew564@gmail.com> 1680297622 -0400
    
    Add logo
    
    Fix bug in obj_loader
    
    began Noise Functions
    
    Added more noise function
    
    testing squashing
    
    changed noise functions to use vectors
    
    seperated into header and src
    
    testing squashing
    
    changed noise functions to use vectors
    
    testing squashing

---
+++ include/glibby/math/noise_functions.h
@@ -0,0 +1,10 @@
+#include <vector>
+namespace glibby
+{
+	std::vector<float> whiteNoise1d(float i);
+	std::vector<float> whiteNoise2d(float i, float j);
+	std::vector<float> whiteNoise3d(float i, float j, float k);
+	std::vector<float> whiteNoise4d(float i, float j, float k, float l);
+	
+
+} 
