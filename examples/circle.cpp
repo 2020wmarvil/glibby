85f420491d2d4d93eafcb3dad6d10488e73729d9
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-02-14 17:33:52 -0500

    Added additional example cases

--- examples/circle.cpp
+++ examples/circle.cpp
@@ -6,8 +6,13 @@
 int main()
 {
 	glibby::Circle2D c1(Point2D(1, 1), 5);
-	glibby::Circle2D c2(0, 0, 5);
+	glibby::Circle2D c2(0, 0, 1);
 
 	float distance = p1.distance(p2);
-	std::cout << distance << std::endl;
+	std::cout << "Distance: " << distance << std::endl;
+	std::cout << "Area: " << c2.area() << std::endl;
+	std::cout << "Perimeter: " << c2.perimeter() << std::endl;
+
+
+
 }
