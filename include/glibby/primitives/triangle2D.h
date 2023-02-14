485eb92f2d53b35cacc0f087f181afbe37213f8b
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-02-14 17:32:36 -0500

    Created constructors and implemented area + perimeter functions

--- include/glibby/primitives/triangle2D.h
+++ include/glibby/primitives/triangle2D.h
@@ -1,15 +1,20 @@
 #pragma once
+#include "glibby/primitives/point2D.h"
 
 namespace glibby
 {
     struct Triangle2D
     {
+    public:
+        //Constructors
+        Triangle2D();
+        Triangle2D(float _x1, float _y1, float _x2, float _y2, float _x3, float _y3);
+        Triangle2D(const Point2D& _p1, const Point2D& _p2, const Point2D& _p3);
+
         float area() const;
         float perimeter() const;
-
-        float x;
-        float y;
-        float r;
+    private:
+        Point2D p1, p2, p3;
     };
 
     float Distance(const Triangle2D& t1, const Triangle2D& t2);
--- src/primitives/triangle2D.cpp
+++ src/primitives/triangle2D.cpp
@@ -3,17 +3,43 @@
 
 namespace glibby
 {
+    Triangle2D::Triangle2D() {
+        p1 = Point2D();
+        p2 = Point2D();
+        p3 = Point2D();
+    }
+    Triangle2D::Triangle2D(float _x1, float _y1, float _x2, float _y2, float _x3, float _y3) {
+        p1 = Point2D(_x1, _y1);
+        p2 = Point2D(_x2, _y2);
+        p3 = Point2D(_x3, _y3);
+
+    }
+
+    Triangle2D::Triangle2D(const Point2D& _p1, const Point2D& _p2, const Point2D& _p3) {
+        p1 = p1;
+        p2 = p2;
+        p3 = p3;
+    }
+
     float Triangle2D::area() const {
-        return -1;
+        float a = p1.distance(p2);
+        float b = p2.distance(p3);
+        float c = p3.distance(p1);
+        float s = (a + b + c) / 2; //Heron's formula
+        return std::sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
     float Triangle2D::perimeter() const {
-        return -1;
+        float a = p1.distance(p2);
+        float b = p2.distance(p3);
+        float c = p3.distance(p1);
+        return a + b + c;
     }
 
     float Distance(const Triangle2D& t1, const Triangle2D& t2){
-        float deltaX = t1.x - t2.x;
-		float deltaY = t1.y - t2.y;
-		return std::sqrt(deltaX * deltaX + deltaY * deltaY);
+        //float deltaX = t1.x - t2.x;
+		//float deltaY = t1.y - t2.y;
+		//return std::sqrt(deltaX * deltaX + deltaY * deltaY);
+        return -1;
     }
 }
