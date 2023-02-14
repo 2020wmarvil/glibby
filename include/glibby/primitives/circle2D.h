a6523e0c6d9e198ab0a2bc40254df1f8d7163598
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-02-14 17:33:38 -0500

    Fixed indentation

--- include/glibby/primitives/circle2D.h
+++ include/glibby/primitives/circle2D.h
@@ -6,23 +6,23 @@ namespace glibby
 {
     struct Circle2D
     {
-        public:
-            //Constructors
-            Circle2D();
-            Circle2D(float _x, float _y, float _r);
-            Circle2D(const Point2D& _c, float _r);
+    public:
+        //Constructors
+        Circle2D();
+        Circle2D(float _x, float _y, float _r);
+        Circle2D(const Point2D& _c, float _r);
 
-            //Accessors
-            const Point2D& get_center() const {return c;}
-            float get_radius() const {return r;}
+        //Accessors
+        const Point2D& get_center() const {return c;}
+        float get_radius() const {return r;}
 
-            float area() const;
-            float perimeter() const;
+        float area() const;
+        float perimeter() const;
 
-        private:
-            //Member Variables
-            Point2D c;
-            float r;
+    private:
+        //Member Variables
+        Point2D c;
+        float r;
     };
 
 }
