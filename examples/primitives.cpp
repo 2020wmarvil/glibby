49c43c95db14d9d63aa0a008be893a5af0ce680a
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-02-17 17:18:13 -0500

    Renamed + added additional examples

--- examples/primitives.cpp
+++ examples/primitives.cpp
@@ -5,13 +5,16 @@
 
 int main()
 {
-	glibby::Circle2D c1(Point2D(1, 1), 5);
+	glibby::Circle2D c1(glibby::Point2D(1, 1), 5);
 	glibby::Circle2D c2(0, 0, 1);
 
-	float distance = p1.distance(p2);
+	float distance = c1.get_center().distance(c2.get_center());
+	std::cout << "Circle 1: " << c1 << std::endl;
+	std::cout << "Circle 2: " << c2 << std::endl;
 	std::cout << "Distance: " << distance << std::endl;
 	std::cout << "Area: " << c2.area() << std::endl;
 	std::cout << "Perimeter: " << c2.perimeter() << std::endl;
+	std::cout << std::boolalpha << "Intersect: " << c1.intersects(c2) << std::endl;
 
 
 
