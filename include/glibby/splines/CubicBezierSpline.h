2ff623bc0c1f5909bc9e77baf62e59531fd5cb7d
Hexase <wangandrew564@gmail.com>
2023-02-24 17:43:04 -0500

    finished uniform sampling

--- include/glibby/splines/CubicBezierSpline.h
+++ include/glibby/splines/CubicBezierSpline.h
@@ -5,14 +5,14 @@ namespace glibby
 	class CubicBezierSpline
 	{
 	public:
-		CublicBezierSpline(const Point<T, N>[4]& sourcepoints)
+		CublicBezierSpline(const Point<T, N>[4] sourcepoints)
 		{
-			for{int i = 0; i < 4; i++}
+			for(int i = 0; i < 4; i++)
 			{
 				P[i] = sourcepoints[i];
 			}
 		}
-		Point<T,N> sampleAt(const T& t)
+		Point<T,N> SampleAt(const T& t)
 		{
 			Point<T, N> returnPoint;
 			for (int i = 0; i < N; i++)
@@ -20,15 +20,52 @@ namespace glibby
 				for (int j = 0; j < 4; j++)
 				{
 					returnPoint.points[i] =
-						(1 - t) * (1 - t) * (1 - t) * P[j].points[i] +
-						3 * (1 - t) * (1 - t) * t * +
-						3(1 - t) * t * t +
-						t * t * t 
-						//this is incomplete.
+						P[j].points[i] * (1 - t) * (1 - t) * (1 - t) +
+						P[j].points[i] * 3 * (1 - t) * (1 - t) * t +
+						P[j].points[i] * 3 * (1 - t) * t * t +
+						P[j].points[i] * t * t * t;
 				}
 			}
+			return returnPoint;
 		}
+		Point<T, N> SampleAtUniform(const T& t)
+		{
+			Point<T, N> returnPoint;
+			Point<T, N> point1;
+			Point<T, N> point2;
+			T distance;
+			T distances[20];
+			T totalDistance = 0;
+			T targetDistance;
 
+			for (int i = 0; i < 20; i++)
+			{
+				point1 = sampleAt(i / 20);
+				point2 = sampleAt((i + 1) / 20);
+
+				distance = EuclideanDistance(point1, point2);
+				distances[i] = distance;
+				totalDistance += distance;
+			}
+
+			distance = 0;
+			int i = 0;
+			targetDistance = t * totalDistance;
+
+			while (distance < targetDistance)
+			{
+				distance += distances[i];
+				i++;
+			}
+
+			returnPoint = sampleAt(i / 20);
+
+			return returnPoint;
+		}
+		Point<T, N> getPoint(int i)
+		{
+			return P[i];
+		}
 	private: 
 		Point<T, N> P[4];
 	};
