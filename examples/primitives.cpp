f00db45f9e6253316fff3a382bbf48b26f2c53a4
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-02-28 17:23:32 -0500

    added test cases to check for test 1 of circle with triangle

--- examples/primitives.cpp
+++ examples/primitives.cpp
@@ -3,11 +3,12 @@
 #include "glibby/primitives/triangle2D.h"
 
 #include <iostream>
+#include <assert.h>
 
 int main()
 {
-	glibby::Circle2D c1(glibby::Point2D(1, 1), 5);
-	glibby::Circle2D c2(0, 0, 1);
+	glibby::Circle2D c1(glibby::Point2D(1, 1), 1);
+	glibby::Circle2D c2(0, 0, 5);
 
 	/*CIRCLES*/
 	//Print circle
@@ -26,9 +27,23 @@ int main()
 	std::cout << std::boolalpha << "Intersect: " << c1.intersects(c2) << std::endl;
 
 	/*TRIANGLES*/
-	glibby::Triangle2D t1(glibby::Triangle2D(glibby::Point2D(1, 0), glibby::Point2D(0, 0), glibby::Point2D(0, 1)));
-	glibby::Triangle2D t2(2, 2, 3, 3, 2, 3);
+	glibby::Triangle2D t1(glibby::Triangle2D(glibby::Point2D(2, 2), glibby::Point2D(10, 4), glibby::Point2D(5, 7)));
+	glibby::Triangle2D t2(5, 0, 10, 4, 5, 7);
+	glibby::Triangle2D out_triangle(50, 50, 100, 50, 50, 100);
 
 	std::cout << "Triangle 1: \n" << t1 << std::endl;
 	std::cout << "Triangle 2: \n" << t2 << std::endl;
+
+	/*Circle x Triangle*/
+	//Test 1
+	//https://i.imgur.com/dDMdPOR.png
+	std::cout << "C2 & T1 intersect: " << c2.intersects(t1) << std::endl;
+	//https://i.imgur.com/pebtxsh.png
+	assert(c2.intersects(t1) == true);
+	std::cout << "C2 & T2 intersect: " << c2.intersects(t2) << std::endl;
+	assert(c2.intersects(t2) == true);
+
+	//Out of bounds
+	std::cout << "C2 & Out Triangle intersect: " << c2.intersects(out_triangle) << std::endl;
+	assert(c2.intersects(out_triangle) == false);
 }
