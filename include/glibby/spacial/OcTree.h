6ad6049e040e1f50a4d9b3b8c6cb147dca0e8568
Jacob Long <jacoblong853@gmail.com>
2023-02-28 17:17:11 -0500

    add header file for OcTree

---
+++ include/glibby/spacial/OcTree.h
@@ -0,0 +1,65 @@
+#ifndef OC_TREE_H
+#define OC_TREE_H
+
+#include "glibby/primitives/point.h"
+
+#include <memory>
+#include <vector>
+
+namespace glibby 
+{
+  class OcTreeNode 
+  {
+    public:
+      OcTreeNode(std::shared_ptr<Point3> cen, float width, float height, 
+          int cap);
+
+      bool inside_boundary(Point3* ptr) const;
+      bool intersect_boundary(Point3* center, float width, float height) const;
+
+      std::shared_ptr<Point3> center_;
+      long unsigned capacity_;
+      float width_;
+      float height_;
+      float depth_;
+      bool divided_;
+      std::shared_ptr<OcTreeNode> NWF_;
+      std::shared_ptr<OcTreeNode> NEF_;
+      std::shared_ptr<OcTreeNode> SWF_;
+      std::shared_ptr<OcTreeNode> SEF_;
+      std::shared_ptr<OcTreeNode> NWB_;
+      std::shared_ptr<OcTreeNode> NEB_;
+      std::shared_ptr<OcTreeNode> SWB_;
+      std::shared_ptr<OcTreeNode> SEB_;
+      std::shared_ptr<OcTreeNode> parent_; 
+      std::vector<std::shared_ptr<Point3>> points_;
+  };
+
+  class OcTree 
+  {
+    public:
+      OcTree(std::shared_ptr<Point3> p, float width, float height, 
+          int capacity = 1);
+
+      bool insert(Point3* point);
+      bool contains(Point3* point) const;
+      std::vector<Point3> query(Point3* point, float width, 
+          float height) const;
+
+      int size() const {return size_;};
+
+    private:
+      bool add_point(std::shared_ptr<OcTreeNode> node, Point3* point);
+      void subdivide(std::shared_ptr<OcTreeNode> node);
+      bool search(std::shared_ptr<OcTreeNode> node, Point3* point) const;
+      void search_tree(
+          std::vector<Point3>* points, std::shared_ptr<OcTreeNode> node, 
+          Point3* center, float width, float height) const;
+      
+      std::shared_ptr<OcTreeNode> node_;
+      int capacity_;
+      int size_;
+  };
+}
+
+#endif
