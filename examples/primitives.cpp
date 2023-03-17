63459e29677627f08b08d8097a656d63e3bf1d33
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-03-17 16:51:48 -0400

    Added additional test cases to cover all cases of a triangle and circle intersection

--- examples/primitives.cpp
+++ examples/primitives.cpp
@@ -29,19 +29,33 @@ int main()
 	/*TRIANGLES*/
 	glibby::Triangle2D t1(glibby::Triangle2D(glibby::Point2D(2, 2), glibby::Point2D(10, 4), glibby::Point2D(5, 7)));
 	glibby::Triangle2D t2(5, 0, 10, 4, 5, 7);
+	glibby::Triangle2D t3(glibby::Triangle2D(glibby::Point2D(5, 1), glibby::Point2D(5, -1), glibby::Point2D(10, 0)));
+	glibby::Triangle2D t4(glibby::Triangle2D(glibby::Point2D(0, 1), glibby::Point2D(-1, -1), glibby::Point2D(1, -1)));
+	glibby::Triangle2D t5(glibby::Triangle2D(glibby::Point2D(0, 5), glibby::Point2D(-5, -5), glibby::Point2D(5, -5)));
 	glibby::Triangle2D out_triangle(50, 50, 100, 50, 50, 100);
 
 	std::cout << "Triangle 1: \n" << t1 << std::endl;
 	std::cout << "Triangle 2: \n" << t2 << std::endl;
 
 	/*Circle x Triangle*/
-	//Test 1
 	//https://i.imgur.com/dDMdPOR.png
 	std::cout << "C2 & T1 intersect: " << c2.intersects(t1) << std::endl;
-	//https://i.imgur.com/pebtxsh.png
 	assert(c2.intersects(t1) == true);
+	//https://i.imgur.com/pebtxsh.png
 	std::cout << "C2 & T2 intersect: " << c2.intersects(t2) << std::endl;
 	assert(c2.intersects(t2) == true);
+	//https://i.imgur.com/pLBK527.png
+	std::cout << "C2 & T3 intersect: " << c2.intersects(t3) << std::endl;
+	assert(c2.intersects(t3) == true);
+	//https://i.imgur.com/1LN0na1.png
+	std::cout << "C2 & T4 intersect: " << c2.intersects(t4) << std::endl;
+	assert(c2.intersects(t4) == true);
+	//https://i.imgur.com/GrnUwR6.png
+	std::cout << "C1 & T5 intersect: " << c1.intersects(t5) << std::endl;
+	assert(c1.intersects(t5) == true);
+
+
+
 
 	//Out of bounds
 	std::cout << "C2 & Out Triangle intersect: " << c2.intersects(out_triangle) << std::endl;
