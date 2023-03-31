9404595ad30dea26c82bad37a817aeed5d72435b
Hexase <wangandrew564@gmail.com>
2023-03-31 17:05:10 -0400

    testing squashing

--- include/glibby/math/noise_functions.h
+++ include/glibby/math/noise_functions.h
@@ -1,17 +1,10 @@
 #include <vector>
 namespace glibby
 {
-	std::vector<float> whiteNoise1d(float i)
-	{
-		std::vector<float> returnVector(1);
+	std::vector<float> whiteNoise1d(float i);
+	std::vector<float> whiteNoise2d(float i, float j);
+	std::vector<float> whiteNoise3d(float i, float j, float k);
+	std::vector<float> whiteNoise4d(float i, float j, float k, float l);
+	
 
-		returnVector[0] = (4.9482 * i - (int)(4.9482 * i));
-	}
-	std::vector<float> whiteNoise2d(float i, float j)
-	{
-		std::vector<float> returnVector(2);
-
-		returnVector[0] = (4.9482 * i - (int)(4.9482 * i));
-		returnVector[1] = (5.3267 * i - (int)(5.3267 * i));
-	}
 } 
