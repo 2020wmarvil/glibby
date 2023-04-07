c7e81508828d05655a946c4a7b59cc3e175579d2
Jacob Long <jacoblong853@gmail.com>
2023-04-07 17:12:57 -0400

    update OcTree tests to include iterator

--- tests/spacial_tests.cpp
+++ tests/spacial_tests.cpp
@@ -444,31 +444,31 @@ TEST_CASE("OcTree insert point","[spacial][OcTree]")
   pt2->points[0] = 1.00f;
   pt2->points[1] = 1.00f;
   pt2->points[2] = 1.00f;
-  CHECK(ot1.insert(&(*pt2)));
+  CHECK(ot1.insert(&(*pt2)).first);
 
   std::shared_ptr<glibby::Point3> pt3(new glibby::Point3);
   pt3->points[0] = 1.00f;
   pt3->points[1] = 1.00f;
   pt3->points[2] = 1.00f;
-  CHECK(ot1.insert(&(*pt3)));
+  CHECK(ot1.insert(&(*pt3)).first);
   
   std::shared_ptr<glibby::Point3> pt4(new glibby::Point3);
   pt4->points[0] = 1.49f;
   pt4->points[1] = -1.49f;
   pt4->points[2] = 1.49f;
-  CHECK(ot1.insert(&(*pt4)));
+  CHECK(ot1.insert(&(*pt4)).first);
   
   std::shared_ptr<glibby::Point3> pt5(new glibby::Point3);
   pt5->points[0] = 3.00f;
   pt5->points[1] = -3.00f;
   pt5->points[2] = 3.00f;
-  CHECK_FALSE(ot1.insert(&(*pt5)));
+  CHECK_FALSE(ot1.insert(&(*pt5)).first);
 
   std::shared_ptr<glibby::Point3> pt6(new glibby::Point3);
   pt6->points[0] = -10.00f;
   pt6->points[1] = 10.00f;
   pt6->points[2] = -10.00f;
-  CHECK_FALSE(ot1.insert(&(*pt6)));
+  CHECK_FALSE(ot1.insert(&(*pt6)).first);
 }
 
 TEST_CASE("OcTree remove point","[spacial][OcTree]") 
@@ -483,36 +483,36 @@ TEST_CASE("OcTree remove point","[spacial][OcTree]")
   pt2->points[0] = 1.00f;
   pt2->points[1] = 1.00f;
   pt2->points[2] = 1.00f;
-  CHECK(ot1.insert(&(*pt2)));
+  CHECK(ot1.insert(&(*pt2)).first);
 
   std::shared_ptr<glibby::Point3> pt3(new glibby::Point3);
   pt3->points[0] = 1.00f;
   pt3->points[1] = 1.00f;
   pt3->points[2] = 1.00f;
-  CHECK(ot1.insert(&(*pt3)));
+  CHECK(ot1.insert(&(*pt3)).first);
   
   std::shared_ptr<glibby::Point3> pt4(new glibby::Point3);
   pt4->points[0] = 1.49f;
   pt4->points[1] = -1.49f;
   pt4->points[2] = 1.49f;
-  CHECK(ot1.insert(&(*pt4)));
+  CHECK(ot1.insert(&(*pt4)).first);
   
   std::shared_ptr<glibby::Point3> pt5(new glibby::Point3);
   pt5->points[0] = 3.00f;
   pt5->points[1] = -3.00f;
   pt5->points[2] = 3.00f;
-  CHECK_FALSE(ot1.insert(&(*pt5)));
+  CHECK_FALSE(ot1.insert(&(*pt5)).first);
 
   std::shared_ptr<glibby::Point3> pt6(new glibby::Point3);
   pt6->points[0] = -10.00f;
   pt6->points[1] = 10.00f;
   pt6->points[2] = -10.00f;
-  CHECK_FALSE(ot1.insert(&(*pt6)));
+  CHECK_FALSE(ot1.insert(&(*pt6)).first);
   
   // remove pt2 and check pt3 and pt4 still exist
   CHECK(ot1.remove(&(*pt2)));
-  CHECK(ot1.contains(&(*pt3)));
-  CHECK(ot1.contains(&(*pt4)));
+  CHECK(ot1.contains(&(*pt3)).first);
+  CHECK(ot1.contains(&(*pt4)).first);
 
   CHECK(ot1.remove(&(*pt3)));
   CHECK(ot1.remove(&(*pt4)));
@@ -532,37 +532,37 @@ TEST_CASE("OcTree contains point","[spacial][OcTree]")
   pt2->points[0] = 1.00f;
   pt2->points[1] = 1.00f;
   pt2->points[2] = 1.00f;
-  CHECK(ot1.insert(&(*pt2)));
+  CHECK(ot1.insert(&(*pt2)).first);
 
   std::shared_ptr<glibby::Point3> pt3(new glibby::Point3);
   pt3->points[0] = 1.00f;
   pt3->points[1] = 1.00f;
   pt3->points[2] = 1.00f;
-  CHECK(ot1.insert(&(*pt3)));
+  CHECK(ot1.insert(&(*pt3)).first);
   
   std::shared_ptr<glibby::Point3> pt4(new glibby::Point3);
   pt4->points[0] = 1.49f;
   pt4->points[1] = -1.49f;
   pt4->points[2] = 1.49f;
-  CHECK(ot1.insert(&(*pt4)));
+  CHECK(ot1.insert(&(*pt4)).first);
   
   std::shared_ptr<glibby::Point3> pt5(new glibby::Point3);
   pt5->points[0] = 3.00f;
   pt5->points[1] = -3.00f;
   pt5->points[2] = 3.00f;
-  CHECK_FALSE(ot1.insert(&(*pt5)));
+  CHECK_FALSE(ot1.insert(&(*pt5)).first);
 
   std::shared_ptr<glibby::Point3> pt6(new glibby::Point3);
   pt6->points[0] = -10.00f;
   pt6->points[1] = 10.00f;
   pt6->points[2] = -10.00f;
-  CHECK_FALSE(ot1.insert(&(*pt6)));
+  CHECK_FALSE(ot1.insert(&(*pt6)).first);
   
-  CHECK(ot1.contains(&(*pt2)));
-  CHECK(ot1.contains(&(*pt3)));
-  CHECK(ot1.contains(&(*pt4)));
-  CHECK_FALSE(ot1.contains(&(*pt5)));
-  CHECK_FALSE(ot1.contains(&(*pt6)));
+  CHECK(ot1.contains(&(*pt2)).first);
+  CHECK(ot1.contains(&(*pt3)).first);
+  CHECK(ot1.contains(&(*pt4)).first);
+  CHECK_FALSE(ot1.contains(&(*pt5)).first);
+  CHECK_FALSE(ot1.contains(&(*pt6)).first);
 }
 
 TEST_CASE("OcTree random insertion and check","[spacial][OcTree]") 
@@ -589,8 +589,8 @@ TEST_CASE("OcTree random insertion and check","[spacial][OcTree]")
   }
   for (int i=0; i < valid.size(); i++) 
   {
-    CHECK(ot1.insert(&(*(valid[i]))));
-    CHECK(ot1.contains(&(*(valid[i]))));
+    CHECK(ot1.insert(&(*(valid[i]))).first);
+    CHECK(ot1.contains(&(*(valid[i]))).first);
   }
 
   for (int i=0; i < 25; i++) 
@@ -645,8 +645,8 @@ TEST_CASE("OcTree random insertion and check","[spacial][OcTree]")
   }
   for (int i=0; i < invalid.size(); i++) 
   {
-    CHECK_FALSE(ot1.insert(&(*(invalid[i]))));
-    CHECK_FALSE(ot1.contains(&(*(invalid[i]))));
+    CHECK_FALSE(ot1.insert(&(*(invalid[i]))).first);
+    CHECK_FALSE(ot1.contains(&(*(invalid[i]))).first);
   }
 }
 
@@ -674,8 +674,8 @@ TEST_CASE("OcTree random insertion with larger capacity","[spacial][OcTree]")
   }
   for (int i=0; i < valid.size(); i++) 
   {
-    CHECK(ot1.insert(&(*(valid[i]))));
-    CHECK(ot1.contains(&(*(valid[i]))));
+    CHECK(ot1.insert(&(*(valid[i]))).first);
+    CHECK(ot1.contains(&(*(valid[i]))).first);
   }
 
   for (int i=0; i < 25; i++) 
@@ -730,8 +730,8 @@ TEST_CASE("OcTree random insertion with larger capacity","[spacial][OcTree]")
   }
   for (int i=0; i < invalid.size(); i++) 
   {
-    CHECK_FALSE(ot1.insert(&(*(invalid[i]))));
-    CHECK_FALSE(ot1.contains(&(*(invalid[i]))));
+    CHECK_FALSE(ot1.insert(&(*(invalid[i]))).first);
+    CHECK_FALSE(ot1.contains(&(*(invalid[i]))).first);
   }
 }
 
@@ -754,8 +754,8 @@ TEST_CASE("OcTree find all points in area","[spacial][OcTree]") {
         temp.points[1] = j;
         temp.points[2] = k;
         should_include.push_back(temp);
-        CHECK(ot1.insert(&temp));
-        CHECK(ot1.contains(&temp));
+        CHECK(ot1.insert(&temp).first);
+        CHECK(ot1.contains(&temp).first);
       }
     }
   }
@@ -799,8 +799,8 @@ TEST_CASE("OcTree find all points in randomized area","[spacial][OcTree]") {
     temp.points[1] = distribution(gen);
     temp.points[2] = distribution(gen);
     all_points.push_back(temp);
-    CHECK(ot1.insert(&temp));
-    CHECK(ot1.contains(&temp));
+    CHECK(ot1.insert(&temp).first);
+    CHECK(ot1.contains(&temp).first);
   }
 
   float width = 2*fabs(distribution(gen));
@@ -842,3 +842,102 @@ TEST_CASE("OcTree find all points in randomized area","[spacial][OcTree]") {
   }
 }
 
+TEST_CASE("OcTree iterator testing","[spacial][OcTree]")
+{
+  std::shared_ptr<glibby::Point3> pt1(new glibby::Point3);
+  pt1->points[0] = 0.00f;
+  pt1->points[1] = 0.00f;
+  pt1->points[2] = 0.00f;
+  glibby::OcTree ot1 = glibby::OcTree(pt1,8.00f,8.00f,8.00f);
+
+  // START HERE
+
+  std::vector<glibby::Point3> all_points;
+  std::vector<bool> found;
+
+  float points_x[25] = {0, -2, -2,  2,  2, -2, -2,  2,  2, -3, -3, -1, -1, -3, -3, -1, -1,  1,  1,  3,  3,  1,  1,  3,  3};
+  float points_y[25] = {0, -2,  2,  2, -2, -2,  2, -2,  2, -3, -1, -1, -3,  1,  3,  3,  1,  1,  3,  3,  1, -3, -1, -1, -3};
+  float points_z[25] = {0, -2, -2, -2, -2,  2,  2,  2,  2, -3, -3, -1, -1,  3,  3,  1,  1,  3,  3,  1,  1, -3, -3, -1, -1};
+  
+  glibby::Point3 temp;
+  for (int i=0; i < 25; i++)
+  {
+    temp.points[0] = points_x[i];
+    temp.points[1] = points_y[i];
+    temp.points[2] = points_z[i];
+    all_points.push_back(temp);
+    found.push_back(false);
+    CHECK(ot1.insert(&temp).first);
+    CHECK(ot1.contains(&temp).first);
+  }
+
+  glibby::OcTree::iterator itr = ot1.begin();
+  while (itr != ot1.end()) {
+    std::shared_ptr<const glibby::Point3> temp = *itr;
+    bool contains = false;
+    for (int i=0; i < all_points.size(); i++)
+    {
+      if (fabs(temp->points[0] - all_points[i].points[0]) < 0.001f &&
+          fabs(temp->points[1] - all_points[i].points[1]) < 0.001f &&
+          fabs(temp->points[2] - all_points[i].points[2]) < 0.001f)
+      {
+        contains = true;
+        found[i] = true;
+      }
+    }
+    CHECK(contains);
+
+    itr++;
+  }
+  for (unsigned int i=0; i < found.size(); i++) {
+    CHECK(found[i]);
+  }
+}
+
+TEST_CASE("OcTree random iterator testing","[spacial][OcTree]")
+{
+  std::shared_ptr<glibby::Point3> pt1(new glibby::Point3);
+  pt1->points[0] = 0.00f;
+  pt1->points[1] = 0.00f;
+  pt1->points[2] = 0.00f;
+  glibby::OcTree ot1 = glibby::OcTree(pt1,5.00f,5.00f,5.00f,3);
+
+  std::default_random_engine gen;
+  std::uniform_real_distribution<float>  distribution(-2.5f, 2.5f);
+
+  std::vector<glibby::Point3> all_points;
+  std::vector<bool> found;
+  for (int i=0; i < 100; i++) 
+  {
+    glibby::Point3 temp;
+    temp.points[0] = distribution(gen);
+    temp.points[1] = distribution(gen);
+    temp.points[2] = distribution(gen);
+    all_points.push_back(temp);
+    found.push_back(false);
+    CHECK(ot1.insert(&temp).first);
+    CHECK(ot1.contains(&temp).first);
+  }
+
+  glibby::OcTree::iterator itr = ot1.begin();
+  while (itr != ot1.end()) {
+    std::shared_ptr<const glibby::Point3> temp = *itr;
+    bool contains = false;
+    for (int i=0; i < all_points.size(); i++)
+    {
+      if (fabs(temp->points[0] - all_points[i].points[0]) < 0.001f &&
+          fabs(temp->points[1] - all_points[i].points[1]) < 0.001f &&
+          fabs(temp->points[2] - all_points[i].points[2]) < 0.001f)
+      {
+        contains = true;
+        found[i] = true;
+      }
+    }
+    CHECK(contains);
+
+    itr++;
+  }
+  for (unsigned int i=0; i < found.size(); i++) {
+    CHECK(found[i]);
+  }
+}
